9ce2cec4d3519c022bd3f5c1873dbbdac1b9d2d5
MSMagnum <10648690@uvu.edu>
2026-04-14 20:07:31 -0600

    Contact Compenent Update
    Updated the contact component to have usable links and cleaner style.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,26 +1,57 @@
+import { useState } from 'react';
 import styles from './Contact.module.css';
+import githubLogo from '../../assets/icons/GitHub_Lockup_White.svg';
+import linkedinLogo from '../../assets/icons/LI-Logo.png';
 
 function Contact() {
+  const [copied, setCopied] = useState(false);
+  const email = 'mangummarc@gmail.com';
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(email);
+      setCopied(true);
+
+      setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+    } catch (err) {
+      console.error('Failed to copy email: ', err);
+    }
+  };
+
   return (
-    <section id="contact" className={styles.contact}>
-      <div className="container">
-        <h2 className="section-title">Contact Me</h2>
-        <p className="section-subtext">
-          Feel free to reach out for collaborations or just a friendly hello!
-        </p>
-        <div className={styles.contactInfo}>
-          <a href="mailto:mangummarc@gmail.com" className={styles.contactLink}>
-            Email:
-          </a>
-          <a
-            href="https://www.linkedin.com/in/marc-mangum-094987235/?skipRedirect=true"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.contactLink}
-          >
-            LinkedIn
-          </a>
-        </div>
+    <section className={styles.container} id="contact">
+      <h2 className={styles.title}>Contact Me</h2>
+      <p className={styles.description}>
+        I am currently seeking opportunities in software development and would
+        love to connect. Whether you have a role, a project idea, or just want
+        to talk tech, feel free to reach out.
+      </p>
+      <div className={styles.emailBlock}>
+        <a className={styles.emailLink} href={`mailto:${email}`}>
+          {email}
+        </a>
+        <button onClick={handleCopy} className={styles.copyButton}>
+          {copied ? 'Copied!' : 'Copy Email'}
+        </button>
+      </div>
+      <div className={styles.cardGrid}>
+        <a
+          className={styles.card}
+          href="https://www.linkedin.com/in/marc-mangum-094987235/?skipRedirect=true"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className={styles.cardImg} src={linkedinLogo} alt="LinkedIn" />
+        </a>
+
+        <a
+          className={styles.card}
+          href="https://github.com/MMagnum"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className={styles.cardImg} src={githubLogo} alt="GitHub" />
+        </a>
       </div>
     </section>
   );
